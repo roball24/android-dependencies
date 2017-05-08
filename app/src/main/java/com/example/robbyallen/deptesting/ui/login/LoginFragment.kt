b9b9dd5c3a67c75955f5675dfe36a3bc0e34e5cf
Robert Allen <robby.allen24@gmail.com>
2017-05-08 13:11:00 -0400

    remove butterknife. collecting inputs on button press

--- app/src/main/java/com/example/robbyallen/deptesting/ui/login/LoginFragment.kt
+++ app/src/main/java/com/example/robbyallen/deptesting/ui/login/LoginFragment.kt
@@ -1,17 +1,13 @@
 package com.example.robbyallen.deptesting.ui.login
 
 import android.app.Fragment
-import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.robbyallen.deptesting.R
 import kotlinx.android.synthetic.main.fragment_login.*
-
-/**
- * Created by robbyallen on 5/8/17.
- */
 
 class LoginFragment : Fragment() {
 
@@ -19,4 +15,18 @@ class LoginFragment : Fragment() {
         super.onCreateView(inflater, container, savedInstanceState)
         return inflater!!.inflate(R.layout.fragment_login, container, false)
     }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        loginButton.setOnClickListener { v -> submit(v) }
+    }
+
+    fun submit(v : View){
+        var usernameText = loginUsername.text
+        var passwordText = loginPassword.text
+        Log.d("roball", "button clicked!")
+        Log.d("roball", "username: ${usernameText}")
+        Log.d("roball", "password: ${passwordText}")
+    }
+
 }
